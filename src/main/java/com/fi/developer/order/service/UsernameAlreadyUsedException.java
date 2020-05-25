206fef25d02ed5434dc7f3dead1fbe17da6b48a6
antey13 <shelderdragon@gmail.com>
2020-05-25 10:07:48 +0300

    Initial version of auth generated by JHipster-6.9.0

---
+++ src/main/java/com/fi/developer/order/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package com.fi.developer.order.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
---
+++ src/main/java/com/fi/developer/order/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.fi.developer.order.service.dto;
